0944fd30daeda9310c385564c596bdb00c66bb29
agus-darmawan <wayanagus.dr@gmail.com>
2025-06-07 07:34:56 +0700

    feat: add final cii grafik routes

--- src/services/cii/dailycii.service.ts
+++ src/services/cii/dailycii.service.ts
@@ -32,22 +32,24 @@ export class DailyCIIService {
   }
 
   async getAttainedCiiByMmsi(mmsi: string): Promise<ICIIGrafik | null> {
-    const dailyCII = await this.dailyCiiRepository.getLatestByMmsi(mmsi);
+    const dailyCII = await this.dailyCiiRepository.getByMmsi(mmsi);
     const type = await this.shipRepository.getShipTypeByMmsi(mmsi);
+
     if (dailyCII && dailyCII.cii.length > 0) {
       const ciiData = dailyCII.cii.map((item) => ({
-        ciiRequired: item.cii.ciiRequired,
         ciiAttained: item.cii.ciiAttained,
         timestamp: item.timestamp.toISOString(),
       }));
+
       const ddVector = type?.d
         ? {
-            d1: type.d.d1 * ciiData[0].ciiRequired,
-            d2: type.d.d2 * ciiData[0].ciiRequired,
-            d3: type.d.d3 * ciiData[0].ciiRequired,
-            d4: type.d.d4 * ciiData[0].ciiRequired,
+            d1: type.d.d1 * dailyCII.cii[0].cii.ciiRequired,
+            d2: type.d.d2 * dailyCII.cii[0].cii.ciiRequired,
+            d3: type.d.d3 * dailyCII.cii[0].cii.ciiRequired,
+            d4: type.d.d4 * dailyCII.cii[0].cii.ciiRequired,
           }
-        : undefined;
+        : { d1: 0, d2: 0, d3: 0, d4: 0 };
+
       return { ciiData, ddVector };
     }
     return null;
